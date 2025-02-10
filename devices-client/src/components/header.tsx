ed2ac7a39a3cd0e9aec75457a54844f26567e8b8
intondiscott <intondiscott@gmail.com>
2025-02-09 20:08:49 -0500

    UI change to table

--- devices-client/src/components/header.tsx
+++ devices-client/src/components/header.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className=" h-16 flex items-center font-black text-3xl mb-2 text-white">
-      <h1 className="border-b-2 ">Device Hub</h1>
+    <div className=" h-16  items-center text-center font-black text-6xl mb-2 text-white">
+      <h1 className="">Device Hub</h1>
     </div>
   );
 };
